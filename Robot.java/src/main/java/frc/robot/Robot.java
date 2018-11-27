60d5c1c61d9378b803c78ceaec6c698c09ff5194
Jonah Lefkoff <jonah_lefkoff@hotmail.com>
2018-11-27 08:18:19 -0700

    want to merge encoder-exploration branch to master.  (#4)
    * Update Robot.java/src/main/java/frc/robot/Robot.java
    
    * working front left encoder
    
    * copied code over to other encoders
    
    * added back left enc

--- Robot.java/src/main/java/frc/robot/Robot.java
+++ Robot.java/src/main/java/frc/robot/Robot.java
@@ -24,6 +24,7 @@ public class Robot extends IterativeRobot {
 	Joystick pad;
     TalonSRX frontLeftRotator, frontRightRotator,backLeftRotator, backRightRotator, frontLeftDriver; 
 	Talon  frontRightDriver, backRightDriver, backLeftDriver;
+	Encoder frontLeftEncoder, frontRightEncoder, backLeftEncoder, backRightEncoder;
 	
 	RobotUtil ru = new RobotUtil();
 
@@ -46,6 +47,11 @@ public class Robot extends IterativeRobot {
 		frontLeftDriver = new TalonSRX(3);
 		backRightDriver = new Talon(0);
 		backLeftDriver = new Talon(2);
+
+		frontLeftEncoder = new Encoder(6, 7, true, Encoder.EncodingType.k4X);
+		frontRightEncoder = new Encoder(8, 9, false, Encoder.EncodingType.k4X);
+		backRightEncoder = new Encoder(4, 5, false, Encoder.EncodingType.k4X);
+		backLeftEncoder = new Encoder(4, 5, false, Encoder.EncodingType.k4X);
     	 
 		double power = 0;
 		
@@ -61,59 +67,60 @@ public class Robot extends IterativeRobot {
 		backLeftDriver.set(power);
 		backRightDriver.set(power);
 
-		Encoder frontLeftEncoder = new Encoder(2, 3, false, Encoder.EncodingType.k4X);
-		frontLeftEncoder.setMaxPeriod(.1);
-		frontLeftEncoder.setMinRate(10);
-		frontLeftEncoder.setDistancePerPulse(5);
-		frontLeftEncoder.setReverseDirection(true);
-		frontLeftEncoder.setSamplesToAverage(7);
-
-		Encoder frontRightEncoder = new Encoder(0, 1, true, Encoder.EncodingType.k4X);
-		frontRightEncoder.setMaxPeriod(.1);
-		frontRightEncoder.setMinRate(10);
-		frontRightEncoder.setDistancePerPulse(5);
-		frontRightEncoder.setReverseDirection(true);
-		frontRightEncoder.setSamplesToAverage(7);
-
-		Encoder backLeftEncoder = new Encoder(6, 7, false, Encoder.EncodingType.k4X);
-		backLeftEncoder.setMaxPeriod(.1);
-		backLeftEncoder.setMinRate(10);
-		backLeftEncoder.setDistancePerPulse(5);
-		backLeftEncoder.setReverseDirection(true);
-		backLeftEncoder.setSamplesToAverage(7);
-
-		Encoder backRightEncoder = new Encoder(4, 5, true, Encoder.EncodingType.k4X);
-		backRightEncoder.setMaxPeriod(.1);
-		backRightEncoder.setMinRate(10);
-		backRightEncoder.setDistancePerPulse(5);
-		backRightEncoder.setReverseDirection(true);
-		backRightEncoder.setSamplesToAverage(7);
     }
     
     public void teleopInit(){
+		frontLeftEncoder.reset();
+		frontRightEncoder.reset();
+		backLeftEncoder.reset();
+		backRightEncoder.reset();
+
+		frontLeftEncoder.setMaxPeriod(.1);
+		frontLeftEncoder.setMinRate(10);
+		frontLeftEncoder.setDistancePerPulse(1/1.2);
+		frontLeftEncoder.setReverseDirection(true);
+		frontLeftEncoder.setSamplesToAverage(7);
+
+		frontRightEncoder.setMaxPeriod(.1);
+		frontRightEncoder.setMinRate(10);
+		frontRightEncoder.setDistancePerPulse(1/1.2);
+		frontRightEncoder.setReverseDirection(true);
+		frontRightEncoder.setSamplesToAverage(7);
+
+		backLeftEncoder.setMaxPeriod(.1);
+		backLeftEncoder.setMinRate(10);
+		backLeftEncoder.setDistancePerPulse(1/1.2);
+		backLeftEncoder.setReverseDirection(true);
+		backLeftEncoder.setSamplesToAverage(7);
+
+		backRightEncoder.setMaxPeriod(.1);
+		backRightEncoder.setMinRate(10);
+		backRightEncoder.setDistancePerPulse(1/1.2);
+		backRightEncoder.setReverseDirection(true);
+		backRightEncoder.setSamplesToAverage(7);
+
+		
+
     }
     		  
     public void teleopPeriodic() {
-		 SmartDashboard.putNumber("Rotation", ahrs.getYaw());	   
+		// frontRightDriver.set(.3);
+		// frontLeftDriver.set(PO, .3);
+		// backLeftDriver.set(.3);
+		// backRightDriver.set(.3);
+
+		// frontRightRotator.set(PO, .4);
+		// frontLeftRotator.set(PO, -.4);
+		// backLeftRotator.set(PO, -.4);
+		// backRightRotator.set(PO, .4);
+		SmartDashboard.putNumber("Front Left Encoder Position", frontLeftEncoder.getDistance());
+		SmartDashboard.putNumber("Front Right Encoder Position", frontRightEncoder.getDistance());
+		SmartDashboard.putNumber("Back Left Encoder Position", backLeftEncoder.getDistance());
+		SmartDashboard.putNumber("Back Right Encoder Position", backRightEncoder.getDistance());
+		SmartDashboard.putNumber("Rotation", ahrs.getYaw());	   
 	}
     
     public void testPeriodic() {
-		SmartDashboard.putNumber("Rotation", ahrs.getYaw());
-		SmartDashboard.putNumber("Encoder FL Position", frontLeftEncoder.getPosition());
-		SmartDashboard.putNumber("Encoder BR Position", backRightEncoder.getPosition());
-		SmartDashboard.putNumber("Encoder FR Position", frontRightEncoder.getPosition());
-		SmartDashboard.putNumber("Encoder BL Position", backLeftEncoder.getPosition());
-
-		frontRightDriver.set(.3);
-		frontLeftDriver.set(PO, .3);
-		backLeftDriver.set(.3);
-		backRightDriver.set(.3);
-
-		frontRightRotator.set(PO, .4);
-		frontLeftRotator.set(PO, -.4);
-		backLeftRotator.set(PO, -.4);
-		backRightRotator.set(PO, .4);
-
     }
     
     public void autonomousInit() {
