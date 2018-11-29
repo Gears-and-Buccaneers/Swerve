a5ac5396970873013eabf8a3743964a5e080fa9f
RC Dawson <frc2972rcdawson@gmail.com>
2018-11-28 17:23:15 -0700

    yuh

--- Robot.java/src/main/java/frc/robot/Robot.java
+++ Robot.java/src/main/java/frc/robot/Robot.java
@@ -20,11 +20,14 @@ import edu.wpi.first.wpilibj.SerialPort;
 public class Robot extends IterativeRobot {
 	
 	final ControlMode PO = ControlMode.PercentOutput;
+	final ControlMode Pos = ControlMode.Position;
     
 	Joystick pad;
-    TalonSRX frontLeftRotator, frontRightRotator,backLeftRotator, backRightRotator, frontLeftDriver; 
+    TalonSRX frontRightRotator,backLeftRotator, backRightRotator; 
 	Talon  frontRightDriver, backRightDriver, backLeftDriver;
-	Encoder frontLeftEncoder, frontRightEncoder, backLeftEncoder, backRightEncoder;
+	Encoder frontRightEncoder, backLeftEncoder, backRightEncoder;
+
+	SwerveSystem flss;
 	
 	RobotUtil ru = new RobotUtil();
 
@@ -36,87 +39,55 @@ public class Robot extends IterativeRobot {
     	
     	//CameraServer.getInstance().startAutomaticCapture();
     	
-    	pad = new Joystick(0);
+    	pad = new Joystick(1);
 		
-		frontLeftRotator = new TalonSRX(4);
 		frontRightRotator = new TalonSRX(6);
 		backLeftRotator = new TalonSRX(5);
 		backRightRotator = new TalonSRX(7);
 
 		frontRightDriver = new Talon(1);
-		frontLeftDriver = new TalonSRX(3);
 		backRightDriver = new Talon(0);
 		backLeftDriver = new Talon(2);
 
-		frontLeftEncoder = new Encoder(6, 7, true, Encoder.EncodingType.k4X);
-		frontRightEncoder = new Encoder(8, 9, false, Encoder.EncodingType.k4X);
+		frontRightEncoder = new Encoder(8, 9, true, Encoder.EncodingType.k4X);
 		backRightEncoder = new Encoder(4, 5, false, Encoder.EncodingType.k4X);
-		backLeftEncoder = new Encoder(4, 5, false, Encoder.EncodingType.k4X);
+		backLeftEncoder = new Encoder(2, 3, true, Encoder.EncodingType.k4X);
     	 
 		double power = 0;
 		
     	// Example SRX : leftWheelTalon.set(PO,power);
 		
-		frontLeftRotator.set(PO, power);
 		frontRightRotator.set(PO, power);
 		backLeftRotator.set(PO, power);
 		backRightRotator.set(PO, power);
 
 		frontRightDriver.set(power);
-		frontLeftDriver.set(PO, power);
 		backLeftDriver.set(power);
 		backRightDriver.set(power);
 
     }
     
     public void teleopInit(){
-		frontLeftEncoder.reset();
 		frontRightEncoder.reset();
 		backLeftEncoder.reset();
 		backRightEncoder.reset();
-
-		frontLeftEncoder.setMaxPeriod(.1);
-		frontLeftEncoder.setMinRate(10);
-		frontLeftEncoder.setDistancePerPulse(1/1.2);
-		frontLeftEncoder.setReverseDirection(true);
-		frontLeftEncoder.setSamplesToAverage(7);
-
-		frontRightEncoder.setMaxPeriod(.1);
-		frontRightEncoder.setMinRate(10);
-		frontRightEncoder.setDistancePerPulse(1/1.2);
-		frontRightEncoder.setReverseDirection(true);
-		frontRightEncoder.setSamplesToAverage(7);
-
-		backLeftEncoder.setMaxPeriod(.1);
-		backLeftEncoder.setMinRate(10);
-		backLeftEncoder.setDistancePerPulse(1/1.2);
-		backLeftEncoder.setReverseDirection(true);
-		backLeftEncoder.setSamplesToAverage(7);
-
-		backRightEncoder.setMaxPeriod(.1);
-		backRightEncoder.setMinRate(10);
-		backRightEncoder.setDistancePerPulse(1/1.2);
-		backRightEncoder.setReverseDirection(true);
-		backRightEncoder.setSamplesToAverage(7);
-
-		
-
+		flss = new SwerveSystem(3,4,6,true);
+		SmartDashboard.putNumber("Initial Position", flss.getPos());
     }
     		  
     public void teleopPeriodic() {
-		// frontRightDriver.set(.3);
-		// frontLeftDriver.set(PO, .3);
-		// backLeftDriver.set(.3);
-		// backRightDriver.set(.3);
+		double y = -1*pad.getRawAxis(1);
+		double x = pad.getRawAxis(0);
+		SmartDashboard.putNumber("Angle", Math.atan(y/x)*180/Math.PI);
+		flss.rotateTo(Math.atan(y/x)*180/Math.PI);
+		//frss.rotateTo(Math.atan(y/x));
+		//blss.rotateTo(Math.atan(y/x));
+		//brss.rotateTo(Math.atan(y/x));
 
-		// frontRightRotator.set(PO, .4);
-		// frontLeftRotator.set(PO, -.4);
-		// backLeftRotator.set(PO, -.4);
-		// backRightRotator.set(PO, .4);
-		SmartDashboard.putNumber("Front Left Encoder Position", frontLeftEncoder.getDistance());
-		SmartDashboard.putNumber("Front Right Encoder Position", frontRightEncoder.getDistance());
-		SmartDashboard.putNumber("Back Left Encoder Position", backLeftEncoder.getDistance());
-		SmartDashboard.putNumber("Back Right Encoder Position", backRightEncoder.getDistance());
+		SmartDashboard.putNumber("Important Rotation", flss.getPos());
+		//SmartDashboard.putNumber("Front Right Encoder Position", frontRightEncoder.getDistance());
+		//SmartDashboard.putNumber("Back Left Encoder Position", backLeftEncoder.getDistance());
+		//SmartDashboard.putNumber("Back Right Encoder Position", backRightEncoder.getDistance());
 		SmartDashboard.putNumber("Rotation", ahrs.getYaw());	   
 	}
     
--- Robot.java/src/main/java/frc/robot/RobotUtil.java
+++ Robot.java/src/main/java/frc/robot/RobotUtil.java
@@ -1,24 +1,6 @@
 package frc.robot;
 
 public class RobotUtil {
-	double initialForwardTime = 300;
-	double toSwitchTime = 2000;
-	double turn90Time = 1250;
-	double centerToSideTime = 1200;
-	double sideToSwitchTime = 300;
-	double liftToSwitchTime = 800;
-	double spitOutTime = 400;
-	double pastSwitchTime = 800;
-	double crossBehindSwitchTime = 2000;
-	double backOfSwitchApproachTime = 400;
-	double restTime = 15000;
-	// It's just borrowing some values.
-	double[] goUpToSwitch = {0, 0, .7, 0};
-	double[] goForward = {.7,.7,.1,0};
-	double[] goBackward = {-.7,-.7,0,0};
-	double[] goSlowForward = {.55,.55,.1,0};
-	double[] goSpitOut = {0,0,.1,1};
-	double[] STOP = {0,0,0,0};
 	
 	public int pos = -1;
 	
@@ -34,9 +16,5 @@ public class RobotUtil {
     	else if (x <= -1) return -1;
     	else return x*Math.abs(x);
     }
-	
-	//-1 is left, 1 is right
-	public double[] turn(int dir) {
-		return new double[] {dir*.55,dir*-.55,0,0};
-	}
+
 }
